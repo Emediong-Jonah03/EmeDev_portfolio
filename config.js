b794da66a004537256a8845c93e2c0ff15aefbb5
Emediong <emediongjonah66@gmail.com>
2025-08-07 10:36:47 +0100

    git ignored files

---
+++ config.js
@@ -0,0 +1,6 @@
+window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-5FG3WK5H68');
+    
+const config = "https://www.googletagmanager.com/gtag/js?id=G-5FG3WK5H68"
